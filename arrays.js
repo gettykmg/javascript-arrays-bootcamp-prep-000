e3a7f7b3c67666c554303040e3ad366fb8d38109
Keith Getty <keithgetty@yahoo.com>
2017-09-14 01:20:29 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,9 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop()
   return array
 }
+
+function removeElementFromEndOfArray(array){
+   return array.slice(0, array.length - 1)
+ }
+ function removeElementFromEndOfArray(array) {
+   return array.slice(0, array.length - 1)
