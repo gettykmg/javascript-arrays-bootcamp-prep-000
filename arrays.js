a7c4c7cbc5db43748be81b6e3276605a90ae0bdf
Keith Getty <keithgetty@yahoo.com>
2017-09-14 01:16:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -26,3 +26,12 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element) {
   array.shift()
   return array
 }
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1)
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop()
+  return array
+}
