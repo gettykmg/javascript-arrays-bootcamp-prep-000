4a554551f9f099e928ff7ce8d4df0c9857d431b7
Keith Getty <keithgetty@yahoo.com>
2017-09-14 01:23:36 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,7 @@ function removeElementFromEndOfArray(array){
    return array.slice(0, array.length - 1)
  }
  function removeElementFromEndOfArray(array) {
-   return array.slice(0, array.length - 1) 
+   return array.slice(0, array.length - 1)
  }
 
 
